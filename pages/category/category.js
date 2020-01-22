bcf1d2279eeaeeeb8ae45fe56c427703aa53e5f5
刘宇超 <282651244@qq.com>
2020-01-22 16:12:20 +0800

    first

---
+++ pages/category/category.js
@@ -0,0 +1,83 @@
+// pages/category/category.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    menuList:[
+      {
+        id:1,
+        name:'董子原则'
+      },
+      {
+        id: 2,
+        name: '董子论LOL'
+      },
+      {
+        id: 3,
+        name: '董子杂谈'
+      },
+      {
+        id: 4,
+        name: '三颜二拍'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
